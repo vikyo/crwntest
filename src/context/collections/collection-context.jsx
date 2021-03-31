ba7ff3fc7748fef2b0840fba5066a63b3aa1330c
vikas <vikashc939@gmail.com>
2021-03-31 21:24:38 +0530

    crwntest commit

---
+++ src/context/collections/collection-context.jsx
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+import SHOP_DATA from "./shop.data";
+
+export const CollectionContext = createContext(SHOP_DATA);
---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+import "./collection.styles.scss";
+import { CollectionContext } from "../../context/collections/collection-context";
+
+const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionContext);
+  const collection = collections[match.params.collectionId];
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CollectionPage;
